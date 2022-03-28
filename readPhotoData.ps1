46e44a3a439c0dbf95dc54b0e031c982d7a109b5
JVanpool <josephinetvanpool@gmail.com>
2022-03-28 18:03:47 -0500

    commit

--- readPhotoData.ps1
+++ readPhotoData.ps1
@@ -3,20 +3,20 @@
 #get date created datetime from a .jpg file (created time)
 #get last modified datetime from a .jpg file (date modified)
 
-#Defining function Get-DateTaken that will get the Date Taken of a JPG file
+##Defining function Get-DateTaken that will get the Date Taken of a JPG file
 function Get-DateTaken
 {
-#Define Parameters of this function 
+##Define Parameters of this function 
   param
   (
-#This will allow the function to be compatible to use in a Pipeline
+##This will allow the function to be compatible to use in a Pipeline
     [Parameter(ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
     [String]
     $Path
   )
    
   begin
-  {
+ {
      $shell = New-Object -COMObject Shell.Application
   }
    
@@ -32,34 +32,15 @@ function Get-DateTaken
   }
 }
  
-#Define the path of the photos
+##Define the path of the photos
 $Path = "D:\OneDrive\Pictures (camera roll)\Test to sort"
-#To get all the .JPG files from the folder specified
+##To get all the .JPG files from the folder specified
 $Files = Get-ChildItem $Path "*.jpg"
-#Define a counter to name Photos taken on the same date with the Counter following the date.  
-#For example 02-02-2021.JPG, 02-02-2021-1.JPG, 02-02-2021-2.JPG
-#$Counter = 1
+
 Foreach($File in $Files){
     $FilePath = $Path + "\" + $File.name
     $DateTaken = Get-ChildItem $Path\$File | Get-DateTaken
-
     return $DateTaken
-   
-   #I don't want to to rename my files, just get date taken
-   #region  #The DateTaken has a format of mm/DD/yyyy.  The '/' will throw an error because it is part of a path.  
-    #we will replace the '/' with a '-'
-   # $NewName = ($DateTaken.DateTaken.substring(0,$DateTaken.DateTaken.IndexOf(' '))).replace('/','-') + ".jpg"
-   # If(-Not(Test-Path $Path\$NewName)){
-   #     Rename-Item -Path $FilePath -NewName $NewName
-   #     Write-Progress "$FilePath has been renamed to $NewName."
-    #}
-    #Else{
-    #    $NewName = ($DateTaken.DateTaken.substring(0,$DateTaken.DateTaken.IndexOf(' '))).replace('/','-') + "-" + $counter + ".jpg"
-    #    Rename-Item -Path $FilePath -NewName $NewName
-    #    Write-Progress "$FilePath has been renamed to $NewName."
-    #    $Counter = $Counter + 1
-   # }
 }
-
 
 
